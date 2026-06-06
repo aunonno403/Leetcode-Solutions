a153122f60139cd523227f198cd6294a8ed30c16
Aunonno <aunonnofarhan9@gmail.com>
2026-06-06 23:09:14 +0600

    Add implementation for Koko Eating Bananas problem

---
+++ 875-Koko_Eating_Bananas.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+
+        def feasible(speed) -> bool:
+            # use integer division to compute ceil(pile/speed)
+            return sum((pile - 1) // speed + 1 for pile in piles) <= h
+
+        left, right = 1, max(piles)
+        while left < right:
+            mid = left + (right - left) // 2
+            if feasible(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
